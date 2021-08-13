d2e4c68f810cf5f926e930e72212939d31ed87cf
ggjulio <juligonz@student.42.fr>
2021-08-13 16:19:35 +0200

    .

--- includes/http/ServerConfig.hpp
+++ includes/http/ServerConfig.hpp
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/13 15:01:14 by juligonz          #+#    #+#             */
-/*   Updated: 2021/08/13 15:13:31 by juligonz         ###   ########.fr       */
+/*   Updated: 2021/08/13 16:16:47 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,38 @@
 #define SEREVER_CONFIG_HPP
 
 #include <vector>
+#include <map>
 #include <string>
+#include <cctype>
 
 
 namespace config
 {
-	
-	struct Server
+struct Server
+{
+	struct Host
 	{
-		struct Listen
-		{
-			std::string host;
-			int port;
-		};
-		
-		struct Location
-		{
-			bool autoindex;
-		};
-		
-		std::vector<Listen> 	listens;
-		std::string				root;		
-		std::string				serverName;		
+		std::string host;
+		int			port;
 	};
-	
 
-	std::vector<Server> servers;	
+	struct Location
+	{
+		bool	autoindex;
+		Host	fastCgiPass;
+		
+	};
+
+	std::vector<Host> 					listens;
+	std::string							serverName;
+	std::string							root;		
+	std::vector<std::string> 			indexes;
+	std::vector<u_short, std::string> 	errors;
 };
 
-}
+std::vector<Server> servers;	
+
+
+}; /* namespace config */
 
 #endif
