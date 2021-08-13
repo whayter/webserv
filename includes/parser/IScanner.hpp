8a5561f3c1e4d7b5fd57149f628a5af7f6e8ba05
ggjulio <juligonz@student.42.fr>
2021-08-13 18:32:46 +0200

    .

---
+++ includes/parser/IScanner.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IScanner.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/13 18:01:37 by juligonz          #+#    #+#             */
+/*   Updated: 2021/08/13 18:07:15 by juligonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ISCANNER_HPP
+#define ISCANNER_HPP
+
+namespace parser {
+
+
+class IScanner
+{
+public:
+	virtual ~IScanner(){};
+
+	virtual char	peek(void)		= 0;
+	virtual int		getLine(void)	= 0;
+	virtual int		getColumn(void)	= 0;
+	virtual bool	eof(void)		= 0;
+	
+	virtual char	get() = 0;
+	virtual char	unget() = 0;
+
+}; /* Interface IScanner */
+
+} /* namespace parser */
+
+#endif /* ISCANNER_HPP */
--- includes/parser/ScannerFD.hpp
+++ includes/parser/ScannerFD.hpp
@@ -1,55 +1,63 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Scanner.hpp                                        :+:      :+:    :+:   */
+/*   ScannerFD.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/31 21:45:53 by juligonz          #+#    #+#             */
-/*   Updated: 2021/08/13 18:19:02 by juligonz         ###   ########.fr       */
+/*   Updated: 2021/08/13 18:19:27 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCANNER_HPP
-#define SCANNER_HPP
+#ifndef SCANNER_FD_HPP
+#define SCANNER_FD_HPP
 
 #include <istream>
 #include "IScanner.hpp"
 
 namespace parser {
 
-class Scanner:  public IScanner
+class ScannerFD:  public IScanner
 {
 public:
-	Scanner(std::istream &inputStream);
-	~Scanner();
+	ScannerFD(int fd);
+	~ScannerFD();
 
-	/// Peek the next character
-	inline char	peek(void)		{ return _inStream.peek(); }
 	inline int	getLine(void)	{ return _line; }
 	inline int	getColumn(void)	{ return _column; }
-	inline bool	eof(void)		{ return _inStream.eof(); }
-	
+
+	/// Peek the next character
+	char		peek(void);
+	bool		eof(void);
+
 	/// @brief Get the next character
 	char		get();
 	/// @brief Decrease stream index by one character.
 	char		unget();
 
+
+
+	static const size_t bufferSize = 500;
 private:
-	Scanner();
+	ScannerFD();
 
 	/// @brief Increment to next character, and increment line/column count 
 	void		moveForward();
 	/// @brief Decrement to previous character, and decrement line/column count 
 	void		moveBackward();
 
+
 	int				_line;
 	int				_column;
 	int				_lastColumn;
-	std::istream&	_inStream;
 	char			_c;
-}; /* class Scanner */
+	
+	char 			_buffer[bufferSize + 1];
+	size_t			_idx;
+	int				_fd;
+}; /* class ScannerFD */
 
 } /* namespace parser */
 
-#endif /* SCANNER_HPP */
+#endif /* SCANNER_FD_HPP */
