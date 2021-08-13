87c40197ed9a118fb16cc8242bc36ccb7600f6ad
whayter <hwinston@student.42.fr>
2021-08-13 15:08:04 +0200

    main

--- src/main.cpp
+++ src/main.cpp
@@ -6,50 +6,46 @@
 /*   By: hwinston <hwinston@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/30 14:07:41 by hwinston          #+#    #+#             */
-/*   Updated: 2021/08/09 12:03:25 by hwinston         ###   ########.fr       */
+/*   Updated: 2021/08/13 15:07:44 by hwinston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fstream>
-// #include "Scanner.hpp"
-// #include "ScannerHttpRequest.hpp"
-#include "ParserHttpRequest.hpp"
-#include "HttpRequest.hpp"
+#include "server/ServerHandler.hpp"
 
 int main(int ac, char** av)
 {
-	(void)ac;
-	(void)av;
-
-	std::ifstream req ("./http_requests/ex2");
-
-	// parser::http::ScannerHttpRequest scanner(req);
-
-	// while (true)
+	// if (ac != 2)
 	// {
-	// 	parser::http::Token t = scanner.getToken();
-	// 	std::cout << t << std::endl;
-	// 	if (t.kind == parser::http::ScopedEnum::kEndOfInput)
-	// 		break;
-	// }
-	// std::cout << std::endl;
-	HttpRequest resquest = parser::http::ParserHttpRequest::create(req);
-
-	
-	// server serv;
-	// long socket;
-
-	// if (!serv.init())
+	// 	std::cerr << "Error: ";
+	// 	std::cerr << "a configuration file is expected as the only parameter.";
+	// 	std::cerr << std::endl;
 	// 	return -1;
-	// while (1)
-	// {
-	// 	std::cout << "\n--- Waiting for new connection ---\n" << std::endl;
-	// 	if (!(socket = serv.accept_connection()))
-	// 		return -1;
-	// 	if (!serv.read_from_socket(socket))
-	// 		return -1;
-	// 	serv.say_hello(socket);
-	// // 	serv.close_socket(socket);
 	// }
+	// if (config = parseConfigFile(av[1]))
+	// {
+	// 	ServerHandler sh(config);
+	// 	sh.run();
+	// }
+	// else
+	// {
+	// 	std::cerr << "Error: ";
+	// 	std::cerr << "something is wrong in the configuration file.";
+	// 	std::cerr << std::endl;
+	// 	return -1;
+	// }
+	// return 0;
+
+	/* temporary */
+
+	std::vector<int> ports;
+	ports.push_back(8080);
+	ports.push_back(8081);
+	ports.push_back(8082);
+	ports.push_back(8083);
+
+	ServerHandler sh(ports);
+	sh.run();
 	return 0;
+
+	/* end temporary */
 }
