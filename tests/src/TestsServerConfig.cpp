22ed47d6c5635819f3e4f678c5dcd0689d322867
ggjulio <juligonz@student.42.fr>
2021-08-19 19:07:33 +0200

    wip autoindex parsing

--- tests/src/TestsServerConfig.cpp
+++ tests/src/TestsServerConfig.cpp
@@ -63,8 +63,11 @@ TEST_CASE( "ServerConfig1 - ./config_files/testParser.conf", "[class][ServerConf
 
 	// autoindex directive
 	CHECK( config.getServer(0).autoindex == false );
-	CHECK( config.getServer(0).locations[0].autoindex == false);
+	CHECK( config.getServer(0).locations[0].autoindex == true);
 	CHECK( config.getServer(0).locations[1].autoindex == false);
-	CHECK( config.getServer(0).locations[2].autoindex == false);
+	CHECK( config.getServer(0).locations[2].autoindex == true);
 
+	CHECK( config.getServer(1).autoindex == true );
+	CHECK( config.getServer(1).locations[0].autoindex == true);
+	CHECK( config.getServer(1).locations[1].autoindex == false);
 }
