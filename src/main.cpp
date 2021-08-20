0254414f47f3fa369467115fe583b05198daa1a2
whayter <hwinston@student.42.fr>
2021-08-20 19:40:26 +0200

    main

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hwinston <hwinston@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/30 14:07:41 by hwinston          #+#    #+#             */
-/*   Updated: 2021/08/19 18:02:06 by juligonz         ###   ########.fr       */
+/*   Updated: 2021/08/20 19:39:57 by hwinston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ServerConfig.hpp"
-#include "Server.hpp"
+#include "ServerHandler.hpp"
 
+#include <iostream>
+#include <iomanip>
 #include <vector>
 
 void parseArgs(int ac, char **av)
@@ -32,10 +34,58 @@ void parseArgs(int ac, char **av)
 	}
 }
 
+void solidLine(int width, char ornament)
+{
+	std::cout << std::setw(width) << std::setfill(ornament);
+	std::cout << ornament << std::endl;
+}
+
+void emptyLine(int width, char ornament)
+{
+	std::cout << ornament << std::setw(width - 2) << std::setfill(' ');
+	std::cout << ' ' << ornament << std::endl;
+}
+
+void textLine(int width, char ornament, std::string text)
+{
+	int fillWidth = (width - 2 - text.size()) / 2;
+	bool fill = (text.size() % 2 == 0 ? false : true);
+	
+	std::cout << ornament << std::setw(fillWidth) << std::setfill(' ');
+	std::cout << ' ' << text;
+	std::cout << std::setw(fillWidth) << std::setfill(' ');
+	if (fill)
+		std::cout << ' ' << ' ' << ornament << std::endl;
+	else
+		std::cout << ' ' << ornament << std::endl;
+}
+
+void prompt()
+{
+	int		width = 80;
+	char	ornament = '*';
+
+	std::cout << std::endl;
+	solidLine(width, ornament);
+	emptyLine(width, ornament);
+	textLine(width, ornament, "Webserv - another 42 project");
+	emptyLine(width, ornament);
+	textLine(width, ornament, "V 1.0 | August 2020");
+	textLine(width, ornament, "Authors: Juligonz, Hwinston");
+	emptyLine(width, ornament);
+	solidLine(width, ornament);
+	std::cout << std::endl;
+}
+
 int main(int ac, char** av)
 {
 	parseArgs(ac, av);
 	ServerConfig& config = ServerConfig::getInstance();
-	std::vector<uint32_t> serverPorts = config.getPorts();
+	server::ServerHandler sh(config.getPorts());
+	sh.start();
+	prompt();
+	while (true)
+		sh.update();
+	sh.stop(0);
 	return 0;
 }
