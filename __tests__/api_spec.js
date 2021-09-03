dd60c30f91f0d08f854e03663c05a417d98d0a95
ggjulio <juligonz@student.42.fr>
2021-09-03 14:14:17 +0200

    .

--- __tests__/api_spec.js
+++ __tests__/api_spec.js
@@ -1,26 +1,38 @@
 const frisby = require('frisby');
 
 describe('Role api test suite', function() {
-	// var baseURL = 'http://docker.local:8088/ajax/users/roles/';
-	/**---This case test the api response and check the result----*/
-  
-	// frisby.create('Get all roles')
-	// 	  .get(baseURL + 'roles')
-	// 	  .expectStatus(200)
-	// 	  .expectHeader('Content-Type', 'application/json')
-	// 	  .afterJSON(function (response) {          
-	// 		  expect(response.body.method).toBe('roles');
-	// 		  expect(response.body.success).toBe(true);          
-	// 		  expect(response.errors).toBe(false);
-	// 		  expect(response.body.roles).toBeDefined();
-	// 	  })     
-	// 	  .toss()
 
-	it ('should return a status of 200', function () {
+	// GET
+
+	FileReader("");
+
+	it ('GET should return a status of 200', function () {
 		return frisby
 		  .get('http://localhost')
-		  .
 		  .expect('status', 200);
 	  });
+	 
 
-  });
+	// POST
+
+	// it ('POST should return a status of 201 Created', function () {
+	// return frisby
+	// 	.post('http://localhost', {
+	// 	title: 'My New Blog Post',
+	// 	content: '<p>A cool blog post!</p>'
+	// 	})
+	// 	.expect('status', 201);
+	// });
+
+
+	// DELETE
+
+	// it ('DELETE should return a status of 204 No Content', function () {
+	// 	return frisby
+	// 	  .del('http://localhost')
+	// 	  .expect('status', 204);
+	// });
+
+
+
+});
