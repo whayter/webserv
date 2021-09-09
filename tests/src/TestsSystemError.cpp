2417bc576e324e9bcb3db4c1eaf50f62f0f53802
ggjulio <juligonz@student.42.fr>
2021-09-09 15:08:25 +0200

    upd Unit  Tests system_error.hpp

--- tests/src/TestsSystemError.cpp
+++ tests/src/TestsSystemError.cpp
@@ -3,17 +3,23 @@
 #include "system_error.hpp"
 #include "error_code.hpp"
 
-TEST_CASE( "ft::system_error", "[namespace][ft][system_error]" )
+// TEST_CASE( "ft::system_error", "[namespace][ft][system_error]" )
+TEST_CASE( "ft::error_code", "[namespace][ft][error_code]" )
 {
-	CHECK(ft::errc::address_in_use == EADDRINUSE);
+	REQUIRE(ft::errc::address_in_use == EADDRINUSE);
 
-	ft::error_code err = ft::make_error_code(ft::errc::address_in_use);
+	ft::error_code err(ft::errc::address_in_use);
+
+	// member functions test
 	CHECK(err.value() == EADDRINUSE);
 	CHECK(err.message() == "Address already in use");
-	
 	err.assign(EPERM);
 	CHECK(err.value() == EPERM);
-	
 	err.clear();
 	CHECK(err.value() == 0);
+
+	// assign operator test
+	err = ft::make_error_code(ft::errc::bad_message);
+	CHECK(err.value() == EBADMSG);
+	CHECK(err.message() == "Bad message");
 }
