49057f9ac8e4c609243a37440ccb07972c58d3ba
whayter <hwinston@student.42.fr>
2021-09-12 15:12:19 +0200

    ;.

--- src/web/Socket.cpp
+++ src/web/Socket.cpp
@@ -6,13 +6,14 @@
 /*   By: hwinston <hwinston@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 16:38:15 by hwinston          #+#    #+#             */
-/*   Updated: 2021/09/11 00:21:32 by hwinston         ###   ########.fr       */
+/*   Updated: 2021/09/12 15:12:00 by hwinston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Socket.hpp"
 
 #include <string>
+#include <cstring>
 #include <arpa/inet.h>
 
 web::Socket::Socket() {}
