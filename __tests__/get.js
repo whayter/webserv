1b5f98fba3e54a7fbf89462c0874f6eb189e34dc
Julio Gonzalez <gonzalez.julio8959@gmail.com>
2021-10-04 17:45:53 +0200

    jpp

--- __tests__/get.js
+++ __tests__/get.js
@@ -49,7 +49,7 @@ it ('GET /NotExist should return 404.html', function () {
   .expect('bodyContains', 'Seems like your page doesn\'t exist anymore')
 });
 
-let timeout = 15000;
+let timeout = 50000;
 jest.setTimeout(timeout);
 it ('GET teletubbies SHOULD WORK !! :-D ', function () {
   return frisby.timeout(timeout)
